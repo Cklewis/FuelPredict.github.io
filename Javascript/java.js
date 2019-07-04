654e3c603414eee72da829017cdbb63b5f0d7d15
Colby Lewis <ColbyK.Lewis@gmail.com>
2019-07-04 04:26:14 -0500

    LocalStorage Testing Progression

--- Javascript/java.js
+++ Javascript/java.js
@@ -1,16 +1,8 @@
-//Doesn't really work
-submit.onclick = function () {
-    var address = document.getElementById("street1_id").value;
-    document.getElementById("outputAddress").innerText = address;
-    alert("outputAddress");
-}
-
 
 function search() {
-
     var address = document.getElementById("street1_id").value;
-    alert(address);
-    document.getElementById("demo").innerHTML= address;
-
+    localStorage.setItem("street1_id",address);
+    console.log(localStorage);
+    document.getElementById("delivery_id").innerHTML= address;
 
 }
