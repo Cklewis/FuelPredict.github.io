f1bab0c2555cc3328bcae5ce84d1522f94d2d7b9
Colby Lewis <ColbyK.Lewis@gmail.com>
2019-07-04 13:02:18 -0500

    Playing around with LocalStorage.. Testing things

--- Javascript/java.js
+++ Javascript/java.js
@@ -1,8 +1,20 @@
+var link = document.querySelector('link[rel="import"]');
+link.addEventListener('load', function(e) {
+  var importedDoc = link.import;
 
+});
+
+//On Submit click run this function from profile
 function search() {
     var address = document.getElementById("street1_id").value;
-    localStorage.setItem("street1_id",address);
-    console.log(localStorage);
-    document.getElementById("delivery_id").innerHTML= address;
+    window.localStorage.setItem("street1_id",address);
+    console.log(localStorage); //Checking
+    document.getElementById("demo").innerHTML= address;
+}
 
+//On gallon field Click run this function from fuelquote
+function delivered(){
+    var delivery = localStorage.getItem('street1_id').value;
+    document.getElementById("demo").innerHTML= delivery;
+    console.log(delivery);
 }
