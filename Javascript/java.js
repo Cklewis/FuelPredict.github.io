c3be0aba71c6380c9168d97f755102a334c342da
Colby Lewis <ColbyK.Lewis@gmail.com>
2019-07-04 03:30:09 -0500

    Changes to State DB on Profile to meet documented Requirements. Coded more JavaScript tests

--- Javascript/java.js
+++ Javascript/java.js
@@ -1,13 +1,16 @@
 //Doesn't really work
-submit.onclick = function(){
+submit.onclick = function () {
     var address = document.getElementById("street1_id").value;
     document.getElementById("outputAddress").innerText = address;
     alert("outputAddress");
 }
 
 
-function search(ele) {
-    if(event.key === 'Enter') {
-        alert(ele.value);        
-    }
+function search() {
+
+    var address = document.getElementById("street1_id").value;
+    alert(address);
+    document.getElementById("demo").innerHTML= address;
+
+
 }
