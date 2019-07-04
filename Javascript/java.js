bd53a65abb705b0c5e07e88c0256572ca71c2fcd
Colby Lewis <ColbyK.Lewis@gmail.com>
2019-07-04 00:40:09 -0500

    Small changes and updates to profile, fuelquote, and testing out javascript testing

--- Javascript/java.js
+++ Javascript/java.js
@@ -1,19 +1,6 @@
-/* $(function () {
-    $('#datepicker').datepicker({
-        format: "dd/mm/yyyy",
-        autoclose: true,
-        todayHighlight: true,
-        showOtherMonths: true,
-        selectOtherMonths: true,
-        autoclose: true,
-        changeMonth: true,
-        changeYear: true,
-        orientation: "button"
-    });
-}); */
-
-var submit = document.getElementById("submit");
-submit.onclick=function(){
-    var street1_id=document.getElementById("street1_id").nodeValue;
-    document.getElementById("outputAddress").innerText=streed1_id;
+var submit = document.getElementById("form");
+submit.onclick = function(){
+    var address = document.getElementById("street1_id").value;
+    document.getElementById("outputAddress").innerText = address;
+    alert("outputAddress")
 }
