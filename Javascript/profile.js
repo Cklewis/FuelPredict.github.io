63b10c1ca49c0f26513e1e8aa4823be263322c74
Colby Lewis <ColbyK.Lewis@gmail.com>
2019-07-19 11:23:50 -0500

    Update profile.js

--- Javascript/profile.js
+++ Javascript/profile.js
@@ -1,9 +1,4 @@
 "use strict";
-/* var link = document.querySelector('link[rel="import"]');
-link.addEventListener('load', function(e) {
-  var importedDoc = link.import;
-
-}); */
 
 //On Submit click run this function from profile
 function store() {
@@ -11,42 +6,3 @@ function store() {
     window.localStorage.setItem("street1_id",address);
     console.log(localStorage); //Checking
 }
-
-/* //On gallon field Click run this function from fuelquote
-function delivered(){
-
-    var delivery = localStorage.getItem('street1_id').value;
-    document.getElementById("demo").innerHTML= delivery;
-    console.log(delivery);
-} */
-
-
-
-/* function goBack() {
-  window.history.back()
-} */
-
-
-/* document.getElementById("myButton").onclick = function () {
-  location.href = "profile.html";
-}; */
-
-
-
-/* var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appID: "app-id",
-}; */
-
-/* firebase.initializeApp(config);
-
-var ref = firebase.database().ref();                           
-ref.on("value", function(snapshot){
-  output.innerHTML = JSON.stringify(snapshot.val(), null, 2);
-}); */
-
