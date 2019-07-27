7cabd191021e17ce2b53bcb1c483b2dac1dd1fa9
Colby Lewis <ColbyK.Lewis@gmail.com>
2019-07-27 14:36:14 -0500

    added js and commented out functionsfor now

--- Javascript/fqHistory.js
+++ Javascript/fqHistory.js
@@ -1,6 +1,10 @@
-function hist(){
+/* function hist(){
     var x= document.getElementById("address")
     x.setAttribute("td","text");
     x.setAttribute("value",localStorage.getItem('street1_id'))
     var delivery = localStorage.getItem('street1_id').value;
-} 
+}  */
+
+/* function hist(){
+    document.writeln(document.form-fdata.gallons.value)
+} */
