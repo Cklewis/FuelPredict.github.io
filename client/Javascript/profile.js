8f6aa1b1e92f376b3a0c5d0ed1f75e52b92a7d98
Colby Lewis <ColbyK.Lewis@gmail.com>
2019-07-27 18:28:39 -0500

    Changed directory to successfully find files in the client folder for JS and index page.

--- client/Javascript/profile.js
+++ client/Javascript/profile.js
@@ -3,6 +3,6 @@
 //On Submit click run this function from profile
 function store() {
     var address = document.getElementById("street1_id").value;
-    window.localStorage.setItem("street1_id",address);
+    window.localStorage.setItem("street1_id", address);
     console.log(localStorage); //Checking
 }
