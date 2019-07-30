336a14f88bbcb8ab3b998ec0d9c993ecc4fcaefc
Justin Sen <jsen0795@gmail.com>
2019-07-29 21:45:00 -0500

    added requirements.txt

--- server/fuelquoteproject/quotes/forms.py
+++ server/fuelquoteproject/quotes/forms.py
@@ -16,5 +16,5 @@ class FuelQuoteModelForm(forms.ModelForm):
       'delivery_date',
     ]
     widgets = {
-    'delivery_date': DatePickerInput(format='%m-%d-%Y'),
+    'delivery_date': DatePickerInput(format='%m/%d/%Y'),
     }
