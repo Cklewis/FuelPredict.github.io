4a4bd224f1bf6db6479e06d82031d81339f35743
Justin Sen <jsen0795@gmail.com>
2019-07-31 22:29:38 -0500

    temporarily switched db to sqlite3 in settings.py

--- server/fuelquoteproject/fuelquoteproject/settings.py
+++ server/fuelquoteproject/fuelquoteproject/settings.py
@@ -86,11 +86,8 @@ WSGI_APPLICATION = 'fuelquoteproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'afsdb',
-        'USER':'postgres',
-        'PASSWORD':'justin0713',
-        'HOST':'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
